27b7bf9d83e638e1e1ec8b8fa6a36059a3729cd9
Ben Flannery <oflannabhra@gmail.com>
2021-07-21 13:26:14 -0400

    add error handling, automatic retry, extract scryfall query code into a single static function

--- transmute/transmute.py
+++ transmute/transmute.py
@@ -12,6 +12,9 @@ SCRYFALL_ID_KEY = "id"
 SCRYFALL_LANGUAGE_KEY = "lang"
 SCRYFALL_SETCODE_KEY = "set"
 
+class CardNotFoundException(Exception):
+    pass
+
 class Transmute:
 
     def __init__(self, in_file: str, out_file: str) -> None:
@@ -22,6 +25,43 @@ class Transmute:
 
         self.reader = None
         self.write = None
+
+
+    @staticmethod
+    def get_card_details(card_name: str, set_code: str=None):
+        try:
+            url = None
+            if set_code:
+                url = f"{SCRYFALL_API}{card_name}&set={set_code}"
+            else:
+                url = f"{SCRYFALL_API}{card_name}"
+            details = Transmute.query(url=url)
+        except CardNotFoundException as e:
+            # try again without set code
+            print(f"card not found: {card_name}")
+            if set_code:
+                print("retrying without set name")
+                details = Transmute.get_card_details(card_name=card_name)
+            else:
+                raise Exception("Unable to retrieve card details") from e
+        return details  
+
+
+    @staticmethod
+    def query(url: str) -> dict:
+        print(f"querying: {url}")
+        response = requests.get(url)
+        details = response.json()
+
+        if set(['code', 'status']).issubset(set(details.keys())):
+            if details['code'] == 'not_found':
+                print("query failed")
+                raise CardNotFoundException()
+            if details['status'] in range(400, 499, 1):
+                print(f"query failed: {url}")
+                raise Exception(f"{details.status} response from Scryfall: {details.details}")
+
+        return details
 
 
     def validate_or_create_files(self):
@@ -38,29 +78,31 @@ class Transmute:
         in_collection = MTGCollection(input_type)
         out_collection = MTGCollection(output_type)
 
+        total_cards = sum(1 for _ in open(self.infile)) - 1
+        print(f"processing {total_cards} cards\n")
+        
         # open files and begin processing
         with open(self.infile) as input_file, open(self.outfile, 'w') as output_file:
             # setup CSV objects
             self.reader = csv.DictReader(input_file)
             self.writer = csv.DictWriter(output_file, fieldnames=out_collection.output_headers)
             self.writer.writeheader()
-            
+
+
             for row in self.reader:
                 in_collection.current_row = row
                 details = None
                 
+                # print(".", end="", flush=True)
+
                 if out_collection.requires_scryfall:
-                    response = None
+                    details = None
                     try:
-                        url = f"{SCRYFALL_API}{row[in_collection.name_header]}&set={row[in_collection.set_code_header]}"
-                        response = requests.get(url)
-                        details = response.json()
-                        print(json.dumps(details, indent=2))
-                        if details['status'] in range(400, 499, 1):
-                            raise Exception(f"{details.status} response from Scryfall: {details.details}")
-                        sleep(0.2)
+                        details = Transmute.get_card_details(card_name=row[in_collection.name_header], set_code=row[in_collection.set_code_header])
+                        sleep(0.075)
                     except Exception as e:
-                        print(f"{e}")
+                        print(f"failed: {e}")
+                        continue
                 
                 
                 output_dict = self.convert_headers(
